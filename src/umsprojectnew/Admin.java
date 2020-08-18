fec2021b36bf39dd87fc41bee339f1340541db78
Shorouk-Adel <shorokzamel@gmail.com>
2020-08-19 00:18:08 +0200

    first commit

---
+++ src/umsprojectnew/Admin.java
@@ -0,0 +1,50 @@
+package umsprojectnew;
+
+import java.util.Scanner;
+
+public class Admin {
+
+    private String Adminuser;
+    private String Adminpassword;
+    public Scanner in = new Scanner(System.in);
+
+    public Admin() {
+        Adminuser = "admin";
+        Adminpassword = "ums";
+
+    }
+
+    public boolean validating() {
+        String user;
+        String password;
+        System.out.println("enter your user");
+        user = in.next();
+        System.out.println("enter your password");
+        password = in.next();
+        if (user.equals(Adminuser) && password.equals(Adminpassword)) {
+            return true;
+
+        } else {
+            return false;
+        }
+    }
+
+ 
+
+    public void setUser(String user) {
+        this.Adminuser = user;
+    }
+
+    public void setPassword(String password) {
+        this.Adminpassword = password;
+    }
+
+    public String getUser() {
+        return Adminuser;
+    }
+
+    public String getPassword() {
+        return Adminpassword;
+    }
+
+}
